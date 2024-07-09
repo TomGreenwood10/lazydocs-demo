7680c49e1b5eafc784c7f54d0db0480c42917f75
Tom Greenwood <tom@advancinganalytics.co.uk>
2024-07-09 17:14:59 +0100

    add examples

---
+++ use.py
@@ -0,0 +1,4 @@
+from .player import multiply_positive_numbers
+
+
+result = multiply_positive_numbers(5, 10)
---
+++ utils.py
@@ -0,0 +1,28 @@
+"""Example of Google style docstrings.
+
+Demo of doctrings.
+
+Todo:
+    - Add more functions.
+    - Run formatter
+"""
+
+
+def multiply_positive_numbers(a: int, b: int) -> int:
+    """
+    Multiplies 2 numbers.
+
+    Args:
+        a: First number.
+        b: Second number.
+
+    Returns:
+        The product of the inputs.
+
+    Raises:
+        AttributeError: If any inputs are negative.
+    """
+    if a < 0 or b < 0:
+        raise AttributeError("Both numbers should be positive.")
+
+    return a * b
